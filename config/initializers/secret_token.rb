d929b55c9f375ae43ab3aa8668ee9c33b494d520
Mike Korcynski <mkorcy@gmail.com>
2011-09-21 10:36:11 -0400

    Initial empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TdlHydraHead::Application.config.secret_token = '861c5dbf9fbda89c55a59ba1e932f1b03aa79d237e41bb2184b1710b80d58149f197d87a5abe99b0c206ebfbc0b47623360eb5298aa4f002b20292cd42125751'
