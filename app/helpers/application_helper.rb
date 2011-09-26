b2864fc848a6d72c4fa8f1773aaf19a16d2aff48
Mike Korcynski <mkorcy@gmail.com>
2011-09-26 11:40:40 -0400

    adding some old hydrangea methods which need work but provide helper methods for use in the faculty publication model

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+
+  #copied these two method sfrom generic_content_objects_helper.rb
+
+
+ def disseminator_link pid, datastream_name
+    "<a class=\"fbImage\" href=\"#{ datastream_disseminator_url(pid, datastream_name) }\">view</a>"
+  end
+
+   def datastream_disseminator_url pid, datastream_name
+    begin
+      base_url = Fedora::Repository.instance.send(:connection).site.to_s
+    rescue
+      base_url = "http://localhost:8983/fedora"
+    end
+    "#{base_url}/get/#{pid}/#{datastream_name}"
+   end
+
+  def get_google_docs_iframe pid, datastream_name
+    "<iframe src=\"http://docs.google.com/viewer?url=http://dl.dropbox.com/u/43822/tufts-UA005.036.001.00001.pdf&embedded=true\" width=\"600\" height=\"780\" style=\"border: none;\"></iframe>"
+  end
+
 end
