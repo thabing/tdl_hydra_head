7da9db238deeecf368db0ed16631e6c0cbc7a0ca
Mike Korcynski <mkorcy@gmail.com>
2011-09-26 11:39:35 -0400

    changed application controller to use tdl layout instead of default blacklight layout, also added method to override standard css with tdl.css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,18 @@ class ApplicationController < ActionController::Base
    include Blacklight::Controller
   # Please be sure to impelement current_user and user_session. Blacklight depends on 
   # these methods in order to perform user specific actions. 
+  before_filter :add_my_own_assets
+
+  def layout_name
+	"tdl"
+  end
+
+  def add_my_own_assets
+        stylesheet_links << "tdl"
+
+        # You can do something similar with javascript files too:
+        # javascript_includes << "my_js"
+  end
 
   protect_from_forgery
 end
