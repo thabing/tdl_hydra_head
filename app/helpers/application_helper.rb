3b2900d1ed302fbbbc51e4a6f59d5337903972dc
Brian Goodmon <brian.goodmon@tufts.edu>
2011-10-14 10:05:52 -0400

    Added "return" to return statements and added parens to method definitions.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,24 +3,24 @@ module ApplicationHelper
   #copied these two methods from generic_content_objects_helper.rb
 
 
-  def disseminator_link pid, datastream_name
-    raw "<a class=\"fbImage\" href=\"#{ datastream_disseminator_url(pid, datastream_name) }\">view</a>"
+  def disseminator_link(pid, datastream_name)
+    return raw("<a class=\"fbImage\" href=\"#{ datastream_disseminator_url(pid, datastream_name) }\">view</a>")
   end
 
 
-  def datastream_disseminator_url pid, datastream_name
+  def datastream_disseminator_url(pid, datastream_name)
     begin
       base_url = Fedora::Repository.instance.send(:connection).site.to_s
     rescue
       base_url = "http://localhost:8983/fedora"
     end
 
-    raw "#{base_url}/get/#{pid}/#{datastream_name}"
+    return raw("#{base_url}/get/#{pid}/#{datastream_name}")
   end
 
 
-  def get_google_docs_iframe pid, datastream_name
-    raw "<iframe src=\"http://docs.google.com/viewer?url=http://dl.dropbox.com/u/43822/tufts-UA005.036.001.00001.pdf&embedded=true\" width=\"600\" height=\"780\" style=\"border: none;\"></iframe>"
+  def get_google_docs_iframe(pid, datastream_name)
+    return raw("<iframe src=\"http://docs.google.com/viewer?url=http://dl.dropbox.com/u/43822/tufts-UA005.036.001.00001.pdf&embedded=true\" width=\"600\" height=\"780\" style=\"border: none;\"></iframe>")
   end
 
 
@@ -38,14 +38,14 @@ module ApplicationHelper
       result += "</div></fieldset>"
     end
 
-    raw(result)
+    return raw(result)
   end
 
 
   def showImage(pid)
     result = "<img src=""" + datastream_disseminator_url(params[:id], "Basic.jpg") + """/>"
 
-    raw(result)
+    return raw(result)
   end
 
 end
