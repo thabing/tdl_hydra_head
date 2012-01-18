634c4da482c5cf05eadf1f2a6f2c718121c97b25
Mike Korcynski <mkorcy@gmail.com>
2012-01-18 15:19:54 -0500

    top nav bar code

---
+++ public/javascripts/tdl.js
@@ -0,0 +1,32 @@
+function extractPageName(hrefString)
+{
+	var arr = hrefString.split('/');
+	return  (arr.length < 2) ? hrefString : arr[arr.length-1].toLowerCase();
+}
+ 
+function setActiveMenu(arr, crtPage)
+{
+	for (var i=0; i < arr.length; i++)
+	{
+		if(extractPageName(arr[i].href) == crtPage)
+		{
+			if (arr[i].parentNode.tagName != "DIV")
+			{
+				arr[i].className = "current";
+				arr[i].parentNode.className = "current";
+			}
+		}
+	}
+}
+ 
+function setPage()
+{
+	hrefString = document.location.href ? document.location.href : document.location;
+ 
+	if (document.getElementById("navigation") !=null )
+	setActiveMenu(document.getElementById("navigation").getElementsByTagName("a"), extractPageName(hrefString));
+}
+
+$(document).ready(function(){
+  setPage();
+});
