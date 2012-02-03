3a55c504d773c4a5b4505b04e72fbd34fee6de21
Mike Korcynski <mkorcy@gmail.com>
2012-02-02 23:11:13 -0500

    tdldev fixtures for election records and more eads to cover collection descriptions, and modified model name helper to cover election records

--- app/helpers/model_name_helper.rb
+++ app/helpers/model_name_helper.rb
@@ -39,6 +39,8 @@ module ModelNameHelper
         mapped_model_name = "info:fedora/afmodel:TuftsTEI"
       elsif model_name == "info:fedora/cm:Text.RCR"
         mapped_model_name = "info:fedora/afmodel:TuftsRCR"
+      elsif model_name == "info:fedora/cm:VotingRecord"
+        mapped_model_name = "info:fedora/afmodel:TuftsVotingRecord"
       end
 
       if mapped_model_name != ""
