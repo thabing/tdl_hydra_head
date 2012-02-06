717fc01b0c4e6db544e671711cb6f20285e40ee6
Mike Korcynski <mkorcy@gmail.com>
2012-02-06 15:45:09 -0500

    tweaked OM for transcripts

--- app/models/tufts_audio_text_meta.rb
+++ app/models/tufts_audio_text_meta.rb
@@ -8,46 +8,28 @@
   class TuftsAudioTextMeta < ActiveFedora::NokogiriDatastream
 
     set_terminology do |t|
-      t.root(:path => "TEI.2", "xmlns" => "http://www.tei-c.org/ns/2.0")
-      t.teiHeader(:path => "teiHeader")
-      t.fileDesc(:path => "fileDesc")
-      t.titleStmt(:path => "titleStmt")
-      t.title(:path => "title")
-      t.author(:path => "author")
-      t.extent(:path => "extent")
-      t.publicationStmt(:path => "publicationStmt")
-      t.distributor(:path => "distributor")
-      t.address(:path => "address")
-      t.addrLine(:path => "addrLine")
-      t.idno(:path => "idno")
-      t.availability(:path => "availability")
-      t.sourceDesc(:path => "sourceDesc")
-      t.recordingStmt(:path => "recordingStmt")
-      t.recording(:path => "recording")
-      t.equipment(:path => "equipment")
-      t.respStmt(:path => "respStmt")
-      t.resp(:path => "resp")
-      t.name(:path => "name")
-      t.encodingDesc(:path => "encodingDesc")
-      t.editorialDecl(:path => "editorialDecl")
-      t.stdVals(:path => "stdVals")
-      t.classDecl(:path => "classDecl")
-      t.taxonomy(:path => "taxonomy")
-      t.bibl(:path => "bibl")
-      t.profileDesc(:path => "profileDesc")
-      t.creation(:path => "creation")
-      t.date(:path => "date")
-      t.langUsage(:path => "langUsage")
-      t.language(:path => "language")
-      t.particDesc(:path => "particDesc")
-      t.person(:path => "person")
-      t.p(:path => "p")
-      t.text(:path => "text")
-      t.body(:path => "body")
-      t.timeline(:path => "timeline")
-      t.when(:path => "when")
-      t.div1(:path => "div1")
-      t.u(:path => "u")
+      t.root(:path => "TEI.2", :namespace_prefix=> nil,:xmlns => "",:schema=>"http://dca.tufts.edu/schema/tei/tei2.dtd")
+
+      t.teiHeader(:path=>"teiHeader", :namespace_prefix => nil) {
+
+        t.fileDesc(:path=>"fileDesc", :namespace_prefix => nil) {
+          t.titleStmt(:path=>"titleStmt", :namespace_prefix => nil) {
+              t.title(:path => "title",:namespace_prefix => nil)
+              t.author(:path => "author",:namespace_prefix => nil)
+          }
+        }
+      }
+
+      t.text(:path=>"text",:namespace_prefix=>nil) {
+        t.body(:path=>"body",:namespace_prefix=>nil) {
+          t.div1(:path=>"div1",:namespace_prefix=>nil)  {
+            t.u(:path=>"u",:namespace_prefix=>nil)
+            t.speaker(:path=>"u",:namespace_prefix=>nil,:attributes=>{:type=>"who"})
+          }
+        }
+      }
+      t.transcript(:proxy=>[:text,:body,:div1])
+      t.title(:proxy=>[:teiHeader,:fileDesc,:titleStmt,:title])
     end
 
 
