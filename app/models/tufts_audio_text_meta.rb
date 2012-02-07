a335eaec76b23e913e81acc236e60a1d72341737
bgoodm01 <bgoodm01@dhcp-130-64-77-113.medford.tufts.edu>
2012-02-07 16:56:51 -0500

    Progress on TuftsAudioText transcript.  Opinionated Metadata is working, still need to spew nice html.

--- app/models/tufts_audio_text_meta.rb
+++ app/models/tufts_audio_text_meta.rb
@@ -8,28 +8,47 @@
   class TuftsAudioTextMeta < ActiveFedora::NokogiriDatastream
 
     set_terminology do |t|
-      t.root(:path => "TEI.2", :namespace_prefix=> nil,:xmlns => "",:schema=>"http://dca.tufts.edu/schema/tei/tei2.dtd")
+      t.root(:path => "TEI.2", :namespace_prefix => nil, :xmlns => "", :schema => "http://dca.tufts.edu/schema/tei/tei2.dtd")
 
-      t.teiHeader(:path=>"teiHeader", :namespace_prefix => nil) {
-
-        t.fileDesc(:path=>"fileDesc", :namespace_prefix => nil) {
-          t.titleStmt(:path=>"titleStmt", :namespace_prefix => nil) {
-              t.title(:path => "title",:namespace_prefix => nil)
-              t.author(:path => "author",:namespace_prefix => nil)
+      t.teiHeader(:path => "teiHeader", :namespace_prefix => nil) {
+        t.fileDesc(:path => "fileDesc", :namespace_prefix => nil) {
+          t.titleStmt(:path => "titleStmt", :namespace_prefix => nil) {
+              t.title(:path => "title", :namespace_prefix => nil)
+              t.author(:path => "author", :namespace_prefix => nil)
+          }
+        }
+        t.profileDesc(:path => "profileDesc", :namespace_prefix => nil) {
+          t.particDesc(:path => "particDesc", :namespace_prefix => nil) {
+            t.person(:path => "person", :namespace_prefix => nil) {
+              t.id_attr(:path => {:attribute => "id"}, :namespace_prefix => nil)
+              t.role_attr(:path => {:attribute => "role"}, :namespace_prefix => nil)
+              t.p(:path => "p", :namespace_prefix => nil)
+            }
           }
         }
       }
 
-      t.text(:path=>"text",:namespace_prefix=>nil) {
-        t.body(:path=>"body",:namespace_prefix=>nil) {
-          t.div1(:path=>"div1",:namespace_prefix=>nil)  {
-            t.u(:path=>"u",:namespace_prefix=>nil)
-            t.speaker(:path=>"u",:namespace_prefix=>nil,:attributes=>{:type=>"who"})
+      t.text(:path => "text", :namespace_prefix => nil) {
+        t.body(:path => "body", :namespace_prefix => nil) {
+          t.div1(:path => "div1", :namespace_prefix => nil)  {
+            t.u(:path => "u", :namespace_prefix => nil) {
+              t.who_attr(:path => {:attribute => "who"}, :namespace_prefix => nil)
+              t.u_inner(:path => "u", :namespace_prefix => nil)
+            }
           }
         }
       }
-      t.transcript(:proxy=>[:text,:body,:div1])
-      t.title(:proxy=>[:teiHeader,:fileDesc,:titleStmt,:title])
+
+      t.title(:proxy => [:teiHeader, :fileDesc, :titleStmt, :title])
+      t.author(:proxy => [:teiHeader, :fileDesc, :titleStmt, :author])
+
+      t.id_attr(:proxy => [:teiHeader, :profileDesc, :particDesc, :person, :id_attr])
+      t.role_attr(:proxy => [:teiHeader, :profileDesc, :particDesc, :person, :role_attr])
+      t.p(:proxy => [:teiHeader, :profileDesc, :particDesc, :person, :p])
+
+      t.u(:proxy => [:text, :body, :div1, :u])
+      t.who_attr(:proxy => [:text, :body, :div1, :u, :u, :who_attr])
+      t.u_inner(:proxy => [:text, :body, :div1, :u, :u_inner])
     end
 
 
