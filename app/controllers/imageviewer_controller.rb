dc28b35f4d6788ce4aaa7431feba415e6f559512
Mike Korcynski <mkorcy@gmail.com>
2012-02-16 16:51:18 -0500

    changed inheritence

--- app/controllers/imageviewer_controller.rb
+++ app/controllers/imageviewer_controller.rb
@@ -1,8 +1,12 @@
-class ImageviewerController < ApplicationController
+class ImageviewerController < CatalogController
   layout "imageviewer"
   def index
 
+
   end
+
+
+
   def show
     "<h1>blah</h1>"
   end
