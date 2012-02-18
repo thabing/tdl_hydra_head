9c4896ef7f8db82b1045029cb48514943817a9b6
Mike Korcynski <mkorcy@gmail.com>
2012-02-18 13:05:50 -0500

    removed unused helpers from when we first moved the hydrangea code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,29 +1,5 @@
 module ApplicationHelper
 
-  #copied these two methods from generic_content_objects_helper.rb
-
-
-  def disseminator_link(pid, datastream_name)
-    return raw("<a class=\"fbImage\" href=\"#{ datastream_disseminator_url(pid, datastream_name) }\">view</a>")
-  end
-
-
-  def datastream_disseminator_url(pid, datastream_name)
-    begin
-      base_url = Fedora::Repository.instance.send(:connection).site.to_s
-    rescue
-      base_url = "http://localhost:8983/fedora"
-    end
-
-    return raw("#{base_url}/get/#{pid}/#{datastream_name}")
-  end
-
-
-  def get_google_docs_iframe(pid, datastream_name)
-    return raw("<iframe src=\"http://docs.google.com/viewer?url=http://dl.dropbox.com/u/43822/tufts-UA005.036.001.00001.pdf&embedded=true\" width=\"600\" height=\"780\" style=\"border: none;\"></iframe>")
-  end
-
-
   def showMetadataItem(label, tagID, metadataKey)
     return showMetadataItemForDatastream("DCA-META", label, tagID, metadataKey)
   end
