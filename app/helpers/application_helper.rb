eaadefa238530b055f73fee20ac3f9902d48307f
bgoodm01 <bgoodm01@dhcp-130-64-77-113.medford.tufts.edu>
2012-02-29 18:12:39 -0500

    Performance improvement for transcript rendering -- use << instead of += to build string.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -126,8 +126,8 @@ module ApplicationHelper
 
   def showTranscriptFromDatastream(datastream)
     result =  "<div class=\"participant_section\">\n"
-    result += "            <h1 class=\"participant_header\">Interview Participants</h1>\n"
-    result += "            <div class=\"participant_table\">\n"
+    result << "            <h1 class=\"participant_header\">Interview Participants</h1>\n"
+    result << "            <div class=\"participant_table\">\n"
 
     participant_number = 0
     node_sets = @document_fedora.datastreams[datastream].find_by_terms_and_value(:participants)
@@ -139,18 +139,18 @@ module ApplicationHelper
           id = child.attributes["id"]
           role = child.attributes["role"]
           sex = child.attributes["sex"]
-          result += "              <div class=\"participant_row\" id=\"participant" + participant_number.to_s + "\">\n"
-          result += "                <div class=\"participant_id\">" + (id.nil? ? "" : id) + ":</div>\n"
-          result += "                <div class=\"participant_name\">" + child.text + "</div>\n"
-          result += "                <div class=\"participant_role\">" + (role.nil? ? "" : role) + "</div>\n"
-          result += "                <div class=\"participant_sex\">" + (sex.nil? ? "" : sex) + "</div>\n"
-          result += "              </div> <!-- participant_row -->\n"
+          result << "              <div class=\"participant_row\" id=\"participant" + participant_number.to_s + "\">\n"
+          result << "                <div class=\"participant_id\">" + (id.nil? ? "" : id) + ":</div>\n"
+          result << "                <div class=\"participant_name\">" + child.text + "</div>\n"
+          result << "                <div class=\"participant_role\">" + (role.nil? ? "" : role) + "</div>\n"
+          result << "                <div class=\"participant_sex\">" + (sex.nil? ? "" : sex) + "</div>\n"
+          result << "              </div> <!-- participant_row -->\n"
         end
       end
     end
 
-    result += "            </div> <!-- participant_table -->\n"
-    result += "          </div> <!-- participant_section -->\n"
+    result << "            </div> <!-- participant_table -->\n"
+    result << "          </div> <!-- participant_section -->\n"
 
     timepoints = Hash.new
     node_sets = @document_fedora.datastreams[datastream].find_by_terms_and_value(:when)
@@ -161,15 +161,14 @@ module ApplicationHelper
       unless timepoint_id.nil? || timepoint_interval.nil?
         timepoint_id = timepoint_id.value
         timepoint_interval = timepoint_interval.value
-        # result += "<!-- " + timepoint_id + " => " + timepoint_interval + " -->\n"
         timepoints[timepoint_id] = timepoint_interval
       end
     end
 
-    result += "          <div class=\"transcript_section\">\n"
-    result += "            <h1 class=\"transcript_header\">Transcript</h1>\n"
-    result += "            <div class=\"transcript_scrollarea\">\n"
-    result += "              <div class=\"transcript_table\">\n"  
+    result << "          <div class=\"transcript_section\">\n"
+    result << "            <h1 class=\"transcript_header\">Transcript</h1>\n"
+    result << "            <div class=\"transcript_scrollarea\">\n"
+    result << "              <div class=\"transcript_table\">\n"  
 
     node_sets = @document_fedora.datastreams[datastream].find_by_terms_and_value(:u)
 
@@ -193,43 +192,43 @@ module ApplicationHelper
           end
         end
       end
-      result += "                <div class=\"transcript_chunk\" id=\"chunk" + string_total_seconds + "\">\n"
+      result << "                <div class=\"transcript_chunk\" id=\"chunk" + string_total_seconds + "\">\n"
       unless (string_total_seconds == "")
-        result += "                  <div class=\"transcript_row\">\n"
-        result += "                    <div class=\"transcript_speaker\"></div>\n"
-        result += "                    <div class=\"transcript_utterance\">\n"
-        result += "                      <a class=\"transcript_chunk_link\" href=\"javascript:YAHOO.MediaPlayer.play(thisMediaObj.track," + string_milliseconds + ");\">" + string_minutes + ":" + string_just_seconds + "</a>\n"
-        result += "                    </div> <!-- transcript_utterance -->\n"
-        result += "                  </div> <!-- transcript_row -->\n"
+        result << "                  <div class=\"transcript_row\">\n"
+        result << "                    <div class=\"transcript_speaker\"></div>\n"
+        result << "                    <div class=\"transcript_utterance\">\n"
+        result << "                      <a class=\"transcript_chunk_link\" href=\"javascript:YAHOO.MediaPlayer.play(thisMediaObj.track," + string_milliseconds + ");\">" + string_minutes + ":" + string_just_seconds + "</a>\n"
+        result << "                    </div> <!-- transcript_utterance -->\n"
+        result << "                  </div> <!-- transcript_row -->\n"
       end
       node.children.each do |child|
         childName = child.name
         if (childName == "u")
           unless child.attributes.empty?
             who = child.attributes["who"]
-            result += "                  <div class=\"transcript_row\">\n"
-            result += "                    <div class=\"transcript_speaker\">"+ (who.nil? ? "" : who.value) + "</div>\n"
-            result += "                    <div class=\"transcript_utterance\">"+ parseNotations(child) + "</div>\n"
-            result += "                  </div> <!-- transcript_row -->\n"
+            result << "                  <div class=\"transcript_row\">\n"
+            result << "                    <div class=\"transcript_speaker\">"+ (who.nil? ? "" : who.value) + "</div>\n"
+            result << "                    <div class=\"transcript_utterance\">"+ parseNotations(child) + "</div>\n"
+            result << "                  </div> <!-- transcript_row -->\n"
           end
         elsif (childName == "event" || childName == "gap" || childName == "vocal" || childName == "kinesic")
           unless child.attributes.empty?
             desc = child.attributes["desc"]
             unless desc.nil?
-              result += "                  <div class=\"transcript_row\">\n"
-              result += "                    <div class=\"transcript_speaker\">""</div>\n"
-              result += "                    <div class=\"transcript_utterance\"><span class = \"transcript_notation\">["+ desc + "]</span></div>\n"
-              result += "                  </div> <!-- transcript_row -->\n"
+              result << "                  <div class=\"transcript_row\">\n"
+              result << "                    <div class=\"transcript_speaker\">""</div>\n"
+              result << "                    <div class=\"transcript_utterance\"><span class = \"transcript_notation\">["+ desc + "]</span></div>\n"
+              result << "                  </div> <!-- transcript_row -->\n"
             end
           end
         end
       end
-      result += "                </div> <!-- transcript_chunk -->\n"
+      result << "                </div> <!-- transcript_chunk -->\n"
     end
 
-    result += "              </div> <!-- transcript_table -->\n"
-    result += "            </div> <!-- transcript_scrollarea -->\n"
-    result += "          </div> <!-- transcript_section -->"
+    result << "              </div> <!-- transcript_table -->\n"
+    result << "            </div> <!-- transcript_scrollarea -->\n"
+    result << "          </div> <!-- transcript_section -->"
 
     return raw(result)
   end
