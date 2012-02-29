a8fac653e9b7caa3e2dd43197ed494d4bde15134
Mike Korcynski <mkorcy@gmail.com>
2012-02-29 16:02:46 -0500

    missed one on previous commit filtering non existant js

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -271,7 +271,7 @@ module ApplicationHelper
     return "" unless respond_to?(:javascript_includes)
     str = ""
     javascript_includes.collect do |args|
-      if (args.to_a & %w(hydra/hydra-head jquery.form.js spin.min.js)).empty?
+      if (args.to_a & %w(hydra/hydra-head jquery.form.js spin.min.js catalog/show custom)).empty?
         str +=javascript_include_tag(*args)
       end
     end.join("\n")
