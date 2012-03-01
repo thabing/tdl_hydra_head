6d91b077955d9ea2b0432bc2c5629635f6dfcf86
bgoodm01 <bgoodm01@dhcp-130-64-77-113.medford.tufts.edu>
2012-03-01 16:13:13 -0500

    Fixed two bugs in transcript scrolling:   1. after pause/resume transcript scrolled briefly to time 0;   2. transcript wasn't scrolled properly when << or >> buttons were clicked.

--- public/javascripts/audio/transcript_scroller.js
+++ public/javascripts/audio/transcript_scroller.js
@@ -13,18 +13,34 @@ $(document).ready(
 
 
 	function scrollTranscript(progressArray) {
+
+		if (resumeTime != 0) {
+			// This means that the pause button was clicked and now the play button has been clicked.
+			// The default behavior is to start over at time 0;  that behavior has been changed in
+			// audio_player.js by skipping the player to resumeTime (defined in audio_player.js),
+			// but on onProgress event will still be generated for time 0.  Ignore that event
+			// (otherwise the transcript would be scrolled back to the beginning briefly).
+			return;
+		}
+
 		//progressArray includes keys 'elapsed' and 'duration', both in ms
 		var elapsedTime = progressArray['elapsed'];
 		var seconds = Math.floor(elapsedTime / 1000);
 
-		// Only proceed if time is at least one second greater than last time this was called.
+		// Only proceed if the time is different than when this function was last called.
 		if (seconds != lastSeconds) {
 			lastSeconds = seconds;
 
-			var div = document.getElementById("chunk" + seconds);
+			var div = null;
 
-			// Only proceed if a transcript chunk div exists for this number of seconds.
-			if (div != null) {
+			// Search backwards to find the first div that would contain the transcript at timepoint "seconds".
+			while (div == null & seconds > -1) {
+				div = document.getElementById("chunk" + seconds);
+				seconds -= 1;
+			}
+
+			// Found the right div -- scroll to it and highlight it if it isn't already highlighted
+			if (div != null && div != currentlyHighlightedDiv) {
 				if (currentlyHighlightedDiv != null) {
 					currentlyHighlightedDiv.style.backgroundColor = 'white';
 				}
