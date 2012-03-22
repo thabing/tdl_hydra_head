d2e412b01f7776619a5aee0602c5adf507d01991
Mike Korcynski <mkorcy@gmail.com>
2012-03-22 17:16:55 -0400

    add method to be able to check current action

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -9,10 +9,19 @@ class CatalogController < ApplicationController
   # These before_filters apply the hydra access controls
   before_filter :enforce_access_controls
   before_filter :enforce_viewing_context_for_show_requests, :only=>:show
+  before_filter :instantiate_controller_and_action_names
+
   # This applies appropriate access controls to all solr queries
   CatalogController.solr_search_params_logic << :add_access_controls_to_solr_params
   # This filters out objects that you want to exclude from search results, like FileAssets
   CatalogController.solr_search_params_logic << :exclude_unwanted_models
+
+
+  def instantiate_controller_and_action_names
+      @current_action = action_name
+      @current_controller = controller_name
+  end
+
   def enforce_facet_permissions
     return
   end
