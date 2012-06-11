459eeb9863809c565d632855b38136e3421dc557
Mike Korcynski <mkorcy@gmail.com>
2012-06-11 17:06:34 -0400

    changed render facet value to render the span numbercount within the link text per spec

--- app/helpers/blacklight_helper.rb
+++ app/helpers/blacklight_helper.rb
@@ -1,20 +1,26 @@
 module BlacklightHelper
-   include Hydra::BlacklightHelperBehavior
+  include Hydra::BlacklightHelperBehavior
 
-   # currently only used by the render_document_partial helper method (below)
-    def document_partial_name(document)
-      return if document[Blacklight.config[:show][:display_type]].nil?
-      ModelNameHelper.map_model_name(document[Blacklight.config[:show][:display_type]].first).gsub(/^[^\/]+\/[^:]+:/,"").underscore.pluralize
+  # currently only used by the render_document_partial helper method (below)
+  def document_partial_name(document)
+    return if document[Blacklight.config[:show][:display_type]].nil?
+    ModelNameHelper.map_model_name(document[Blacklight.config[:show][:display_type]].first).gsub(/^[^\/]+\/[^:]+:/, "").underscore.pluralize
+  end
+
+  def render_facet_value(facet_solr_field, item, options ={})
+    if item.is_a? Array
+      link_to_unless(options[:suppress_link], item[0], add_facet_params_and_redirect(facet_solr_field, item[0]), :class => "facet_select") + raw("<span class='numbercount'> (" + format_num(item[1]) + ")</span>")
+    else
+      text_for_link = item.value
+      unless options[:suppress_link]
+        #link_to_unless(, , :class=>"facet_select") do
+        link_to add_facet_params_and_redirect(facet_solr_field, item.value), :class => "facet_select" do
+          out = text_for_link
+
+          out +="<span class='numbercount'> "+ format_num(item.hits) +" </span>"
+          out.html_safe
+        end
+      end
     end
-
-   def render_facet_value(facet_solr_field, item, options ={})
-     if item.is_a? Array
-       link_to_unless(options[:suppress_link], item[0], add_facet_params_and_redirect(facet_solr_field, item[0]), :class=>"facet_select") + raw("<span class='facet_count'> (" + format_num(item[1]) + ")</span>")
-     else
-       text_for_link = item.value + " ("+format_num(item.hits) +")"
-       link_to_unless(options[:suppress_link], text_for_link, add_facet_params_and_redirect(facet_solr_field, item.value), :class=>"facet_select")
-       #raw("<span class='facet_count'> (" + format_num(item.hits) + ")</span>"
-     end
-   end
-
+  end
 end
