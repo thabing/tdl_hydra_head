1374b61e143a1b68b254460fba3f663349a9213f
Mike Korcynski <mkorcy@gmail.com>
2012-07-11 17:03:10 -0400

    changed default per page to 25

--- config/initializers/blacklight_config.rb
+++ config/initializers/blacklight_config.rb
@@ -22,7 +22,7 @@ Blacklight.configure(:shared) do |config|
 
   config[:default_solr_params] = {
     :qt => "search",
-    :per_page => 10 
+    :per_page => 25
   }
  
   # solr field values given special treatment in the show (single result) view
