eb340b6a2370e438fdd1600ee3cc82ded37b38f2
Mike Korcynski <mkorcy@gmail.com>
2012-07-12 14:30:04 -0400

    removed unused js for now

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,9 +1,9 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-Blacklight.do_more_facets_behavior = function() {
-  $(Blacklight.do_more_facets_behavior.selector).each(function() {
-    //my own thing!
-  });
-};
-*/
+//Blacklight.do_more_facets_behavior = function() {
+//  $(Blacklight.do_more_facets_behavior.selector).each(function() {
+//    //my own thing!
+//  });
+//};
+
