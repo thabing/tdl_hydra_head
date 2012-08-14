b5403852cfea973b7b16f1b018f1f415ff87057a
bgoodm01 <bgoodm01@dhcp-130-64-77-113.medford.tufts.edu>
2012-08-14 09:45:49 -0400

    Made TuftsAudioText tabs work when javascript is disabled.

---
+++ public/javascripts/audio/tabs.js
@@ -0,0 +1,16 @@
+$(document).ready(
+  function() {
+    // If javascript is enabled, make the tabs work with bootstrap's javascript by updating their
+    // hrefs and setting their data-toggle attributes.  This will allow the tabs to work without
+    // reloading the page (which is the default behavior in case javascript is disabled.)
+
+    var tab1 = $("#tab1"),
+        tab2 = $("#tab2");
+
+    tab1.attr("href", "#1");
+    tab2.attr("href", "#2");
+
+    tab1.attr("data-toggle", "tab");
+    tab2.attr("data-toggle", "tab");
+  }
+);
