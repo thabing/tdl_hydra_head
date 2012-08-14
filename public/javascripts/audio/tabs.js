f48dc30375859047a395c7b6191f49e63fcecfcd
bgoodm01 <bgoodm01@dhcp-130-64-77-113.medford.tufts.edu>
2012-08-14 15:32:29 -0400

    Choose the transcript tab on the TuftsAudioText page whenever the player's "Play" button is clicked.

--- public/javascripts/audio/tabs.js
+++ public/javascripts/audio/tabs.js
@@ -12,5 +12,16 @@ $(document).ready(
 
     tab1.attr("data-toggle", "tab");
     tab2.attr("data-toggle", "tab");
+
+    YAHOO.MediaPlayer.onAPIReady.subscribe(playerReady);
   }
 );
+
+	function playerReady() {
+	  // Choose the transcript tab whenever the play button is clicked.
+		YAHOO.MediaPlayer.onTrackStart.subscribe(chooseTranscriptTab);
+	}
+
+	function chooseTranscriptTab() {
+		$("#tab2").click;
+	}
