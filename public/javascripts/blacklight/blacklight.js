350682e364e7531c2a48dcf50257aa75b7fa5a12
Mike Korcynski <mkorcy@gmail.com>
2012-10-15 11:58:14 -0400

    more facets js and non-js behavior, could already use some refactoring.

--- public/javascripts/blacklight/blacklight.js
+++ public/javascripts/blacklight/blacklight.js
@@ -46,19 +46,19 @@ Blacklight = {};
 
 
 $(document).ready(function() {
-  Blacklight.do_zebra_stripe();  
+  //Blacklight.do_zebra_stripe();
   
-  Blacklight.do_select_submit();
+  //Blacklight.do_select_submit();
   
-  Blacklight.do_more_facets_behavior();
+  //Blacklight.do_more_facets_behavior();
   
-  Blacklight.do_lightbox_dialog();
+  //Blacklight.do_lightbox_dialog();
   
-  Blacklight.do_bookmark_toggle_behavior();
+  //Blacklight.do_bookmark_toggle_behavior();
   
-  Blacklight.do_folder_toggle_behavior();       
+  //Blacklight.do_folder_toggle_behavior();
   
-  Blacklight.do_facet_expand_contract_behavior();
+  //Blacklight.do_facet_expand_contract_behavior();
 });
 
 
@@ -76,8 +76,8 @@ $(document).ready(function() {
     //add ajaxy dialogs to certain links, using the ajaxyDialog widget.
     Blacklight.do_more_facets_behavior = function () {
       $( Blacklight.do_more_facets_behavior.selector ).ajaxyDialog({
-          width: $(window).width() / 2,  
-          chainAjaxySelector: "a.next_page, a.prev_page, a.sort_change"        
+          width: $(window).width() / 2,
+          chainAjaxySelector: "a.next_page, a.prev_page, a.sort_change"
       });
     };
     Blacklight.do_more_facets_behavior.selector = "a.more_facets_link";
@@ -100,7 +100,7 @@ $(document).ready(function() {
       $("a.lightboxLink").ajaxyDialog({
           chainAjaxySelector: false
       });
-      //But make the librarian link wider than 300px default. 
+      //But make the librarian link wider than 300px default.
       $('a.lightboxLink#librarianLink').ajaxyDialog("option", "width", 650);
       //And the email one too needs to be wider to fit the textarea
       $("a.lightboxLink#emailLink").ajaxyDialog("option", "width", 500);
@@ -305,35 +305,35 @@ $(document).ready(function() {
     
 })(jQuery);
 
-      
+
 /* A widget written by jrochkind to make a link or form result in
    an in-window ajaxy dialog, instead of page load, using JQuery UI
-   Dialog widget. 
-      
+   Dialog widget.
+
    This widget is actually hosted at: https://github.com/jrochkind/jquery.uiExt.ajaxyDialog
-   
-   
+
+
    Included in this main file because I just couldn't bare to add yet
    another JS file to our app, until we figure out a good fix those too
-   many JS files are serious page load speed problem. */ 
+   many JS files are serious page load speed problem. */
 
       (function($) {
           var widgetNamespace = "uiExt";
           var widgetName = "ajaxyDialog";
-          
+
           $.widget(widgetNamespace + "." + widgetName, {
               options: {
                   extractTitleSelector: "h1, h2, h3, h4, h5",
                   chainAjaxySelector: "a:not([target]), form:not([target])",
                   closeDialogSelector: "a.dialog-close"
               },
-              
+
               _create: function() {
                   var self = this;
                   var element = self.element[0];
                   if (element.tagName.toUpperCase() == "A") {
-                    $(element).bind("click."+self.widgetName,  function(event, ui) {                        
-                        self._handleClick(); 
+                    $(element).bind("click."+self.widgetName,  function(event, ui) {
+                        self._handleClick();
                         return false;
                     });
                   }
@@ -342,57 +342,57 @@ $(document).ready(function() {
                         self._handleSubmit();
                         return false;
                     });
-                  }                
+                  }
               },
-                        
+
               open: function() {
                 var self = this;
-                var element = self.element[0];                                
-                                
+                var element = self.element[0];
+
                 if ( element.tagName.toUpperCase() == "A") {
                   self._handleClick();
                 } else if (element.tagName.toUpperCase() == "FORM") {
                   self._handleSubmit();
                 }
               },
-              
+
               close: function() {
-                this.dialogContainer().dialog("close"); 
-              },                            
-              
+                this.dialogContainer().dialog("close");
+              },
+
               _handleClick: function() {
                   var self = this;
                   var url = this.element.attr("href");
                   var requestDialog = self.dialogContainer();
-            
+
                   $("body").css("cursor", "progress");
-            
+
                   $.ajax({
                       url: url,
                       dataType: "html",
                       success: function(resp, status, xhr) {
-                        if (xhr.status != 0) {                          
+                        if (xhr.status != 0) {
                           self._loadToDialog(resp);
                         } else {
                           //stupid jquery calling this 'success', it's
                           //network unavailable.
-                          self._displayFailure(url, xhr, status); 
+                          self._displayFailure(url, xhr, status);
                         }
                       },
                       error: function(xhr, msg) {
-                        self._displayFailure(url, xhr, msg); 
+                        self._displayFailure(url, xhr, msg);
                       }
-                  });                                
+                  });
               },
-              
+
               _handleSubmit: function() {
                   var self = this;
                   var form = self.element;
                   var actionUri = form.attr("action");
                   var serialized = form.serialize();
-            
+
                   $("body").css("cursor", "progress");
-            
+
                   $.ajax({
                       url: actionUri,
                       data: serialized,
@@ -404,44 +404,44 @@ $(document).ready(function() {
                         } else {
                           //stupid jquery calling this 'success', it's
                           //network unavailable.
-                          self._displayFailure(url, xhr, status); 
+                          self._displayFailure(url, xhr, status);
                         }
                       },
                       error: function(xhr, msg) {
-                        self._displayFailure(actionUri, xhr, msg); 
+                        self._displayFailure(actionUri, xhr, msg);
                       }
                   });
               },
-              
-              _loadToDialog: function(html_content) {     
+
+              _loadToDialog: function(html_content) {
                   var self = this;
                   var dialog = self.dialogContainer();
                   //Cheesy way to restore it to it's default options, plus
                   //our own local options, since its' a reuseable dialog.
-                  //for now we insist on modal:true. 
-                  dialog.dialog($.extend({}, 
-                                  $.ui.dialog.prototype.options, 
-                                  self.options, 
+                  //for now we insist on modal:true.
+                  dialog.dialog($.extend({},
+                                  $.ui.dialog.prototype.options,
+                                  self.options,
                                   {autoOpen:false, modal:true}
                                 ));
-                                    
-                  if (self._trigger('beforeDisplay', 0, html_content) !== false) {                  
+
+                  if (self._trigger('beforeDisplay', 0, html_content) !== false) {
                     dialog.html( html_content );
-            
+
                     //extract and set title
                     var title;
                     self.options.extractTitleSelector &&
-                      (title = dialog.find(self.options.extractTitleSelector).first().remove().text());                  
-                    title = title || 
+                      (title = dialog.find(self.options.extractTitleSelector).first().remove().text());
+                    title = title ||
                       self.element.attr("title")
                     title && dialog.dialog("option", "title", title);
-                                
+
                     //Make any hyperlinks or forms ajaxified, by applying
-                    //this very same plugin to em, and passing on our options.  
+                    //this very same plugin to em, and passing on our options.
                     if (self.options.chainAjaxySelector) {
                       dialog.find(self.options.chainAjaxySelector).ajaxyDialog(self.options);
                     }
-            
+
                     //Make any links marked dialog-close do so
                     if ( self.options.closeDialogSelector ) {
                       dialog.find(self.options.closeDialogSelector).unbind("click." + widgetName);
@@ -450,42 +450,42 @@ $(document).ready(function() {
                           return false;
                       });
                     }
-            
+
                     dialog.dialog("open");
                   }
                   $("body").css("cursor", "auto");
               },
-              
+
               _displayFailure: function(uri, xhr, serverMsg) {
-                if (  this._trigger("error", 0, {uri:uri, xhr: xhr, serverMsg: serverMsg}) !== false) {                                                           
+                if (  this._trigger("error", 0, {uri:uri, xhr: xhr, serverMsg: serverMsg}) !== false) {
                       var dialog = this.dialogContainer();
-                          
+
                       dialog.html("<div class='ui-state-error' style='padding: 1em;'><p><span style='float: left; margin-right: 0.3em;' class='ui-icon ui-icon-alert'></span>Sorry, a software error has occured.</p><p>" + uri + ": " + xhr.status + " "  + serverMsg+"</p></div>");
-                      dialog.dialog("option", "title", "Sorry, an error has occured.");           
+                      dialog.dialog("option", "title", "Sorry, an error has occured.");
                       dialog.dialog("option", "buttons", {"OK": function() { dialog.dialog("close"); }});
                       dialog.dialog("open");
-                  }           
+                  }
                   $("body").css("cursor", "auto");
               },
-              
-              // The DOM element which has a ui dialog() called on it. 
+
+              // The DOM element which has a ui dialog() called on it.
               // Right now we insist upon modal dialogs, and re-use the same
-              // <div>.dialog() for all of them. It's lazily created here.   
+              // <div>.dialog() for all of them. It's lazily created here.
               // If client calls dialog("destroy") on it, no problem, it'll
-              // be lazily created if it's needed again. 
+              // be lazily created if it's needed again.
               dialogContainer: function() {
                 var existing = $("#reusableModalDialog");
                 if ( existing.size() > 0) {
                   return existing.first();
                 }
                 else {
-                  //single shared element for modal dialogs      
+                  //single shared element for modal dialogs
                   var requestDialog = $('<div id="reusableModalDialog" style="display:none"></div>').appendTo('body').
-                      dialog({autoOpen: false}); 
+                      dialog({autoOpen: false});
                   return requestDialog;
                 }
               }
-              
+
           });
       }(jQuery));
 
--- public/javascripts/tdl.js
+++ public/javascripts/tdl.js
@@ -1,35 +1,77 @@
-function extractPageName(hrefString)
-{
-	var arr = hrefString.split('/');
-	return  (arr.length < 2) ? hrefString : arr[arr.length-1].toLowerCase();
-}
- 
-function setActiveMenu(arr, crtPage)
-{
-	for (var i=0; i < arr.length; i++)
-	{
-		if(extractPageName(arr[i].href) == crtPage)
-		{
-			if (arr[i].parentNode.tagName != "DIV")
-			{
-				arr[i].className = "current";
-				arr[i].parentNode.className = "current";
-                return;
-			}
-		}
-	}
-    setActiveMenu(document.getElementById("navigation").getElementsByTagName("a"), "catalog");
-}
- 
-function setPage()
-{
-	hrefString = document.location.href ? document.location.href : document.location;
- 
-	if (document.getElementById("navigation") !=null )
-	setActiveMenu(document.getElementById("navigation").getElementsByTagName("a"), extractPageName(hrefString));
+function extractPageName(hrefString) {
+    var arr = hrefString.split('/');
+    return  (arr.length < 2) ? hrefString : arr[arr.length - 1].toLowerCase();
 }
 
-$(document).ready(function(){
-  setPage();
+function setActiveMenu(arr, crtPage) {
+    for (var i = 0; i < arr.length; i++) {
+        if (extractPageName(arr[i].href) == crtPage) {
+            if (arr[i].parentNode.tagName != "DIV") {
+                arr[i].className = "current";
+                arr[i].parentNode.className = "current";
+                return;
+            }
+        }
+    }
+    setActiveMenu(document.getElementById("navigation").getElementsByTagName("a"), "catalog");
+}
+
+function setPage() {
+    hrefString = document.location.href ? document.location.href : document.location;
+
+    if (document.getElementById("navigation") != null)
+        setActiveMenu(document.getElementById("navigation").getElementsByTagName("a"), extractPageName(hrefString));
+}
+
+$(document).ready(function () {
+    setPage();
+
+
+    Blacklight.do_more_facets_behavior = function () {
+
+        $("a.more_facets_link").each(function () {
+            //my own thing!
+            $(this).on('click', function (e) {
+                e.preventDefault();
+                $("#more_facets_modal .modal-body").html('');
+                var url = $(this).attr("href");
+
+                $.ajax({
+                    url:url,
+                    dataType:"html",
+                    success:function (resp, status, xhr) {
+                        if (xhr.status != 0) {
+
+                            $("#more_facets_modal .modal-body").html(resp);
+                            var title = $("#more_facets_modal .modal-body h2").text();
+                            $("#more_facets_modal .modal-body h2").hide();
+                            $("#more_facets_modal .modal-header h3").text(title);
+
+
+                            $("#more_facets_modal").modal('show');                // initializes and invokes show immediately
+                        } else {
+                            //stupid jquery calling this 'success', it's
+                            //network unavailable.
+                            //self._displayFailure(url, xhr, status);
+                            alert("fail");
+                        }
+                    },
+                    error:function (xhr, msg) {
+                        alert('false');
+                    }
+                });
+
+                return false;
+            });
+
+        });
+    };
+
+    Blacklight.do_more_facets_behavior();
+
+});
+
+$('#more_facets_link').on('click', function () {
+
 });
 
