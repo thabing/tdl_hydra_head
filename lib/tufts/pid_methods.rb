4cd21ab05d8d16869a32aa2089bc5b7ed137ebbb
Mike Korcynski <mkorcy@gmail.com>
2012-11-16 11:57:56 -0500

    skeleton class for mapping pids for figures

---
+++ lib/tufts/pid_methods.rb
@@ -0,0 +1,19 @@
+module Tufts
+  module ModelMethods
+
+    def self.pid_to_urn(pid)
+      urn = ""
+      urn
+    end
+
+    def self.urn_to_pid(urn)
+      pid=""
+      pid
+    end
+
+    def self.is_pid?(pid)
+      # if this is a urn say no, otherwise say yes
+     # unless pid.
+    end
+  end
+end
