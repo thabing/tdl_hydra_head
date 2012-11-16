5de5f012aaa368555cbdc77a94822cd1dbede286
Mike Korcynski <mkorcy@gmail.com>
2012-11-16 13:36:08 -0500

    is pid implementation and urn to pid

--- lib/tufts/pid_methods.rb
+++ lib/tufts/pid_methods.rb
@@ -1,5 +1,5 @@
 module Tufts
-  module ModelMethods
+  module PidMethods
 
     def self.pid_to_urn(pid)
       urn = ""
@@ -7,13 +7,19 @@ module Tufts
     end
 
     def self.urn_to_pid(urn)
+      if is_pid?(urn)
+        return urn
+      end
       pid=""
+      index_of_colon = urn.rindex(':')
+      pid = "tufts" + urn[index_of_colon,urn.length]
       pid
     end
 
     def self.is_pid?(pid)
       # if this is a urn say no, otherwise say yes
      # unless pid.
+     !pid.include? 'central'
     end
   end
 end
