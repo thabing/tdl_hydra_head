f59cc26e3554cfc7000973004bff17b31af039b0
Mike Korcynski <mkorcy@gmail.com>
2012-11-29 17:08:17 -0500

    hack override of advanced search to handle year end and year start

---
+++ config/initializers/blacklight_advanced_search_override.rb
@@ -0,0 +1,35 @@
+BlacklightAdvancedSearch::RenderConstraintsOverride.module_eval do
+
+
+#Over-ride of Blacklight method, provide advanced constraints if needed,
+# otherwise call super. Existence of an @advanced_query instance variable
+# is our trigger that we're in advanced mode.
+  def render_constraints_query(my_params = params)
+    if (@advanced_query.nil? || @advanced_query.keyword_queries.empty?)
+      return super(my_params)
+    else
+      content = ""
+      rack_req = @_env['rack.request.query_hash']
+      unless rack_req['year_start'].nil? || rack_req['year_start'].empty?
+        @advanced_query.keyword_queries['year_start'] = rack_req['year_start']
+      end
+      unless rack_req['year_end'].nil? || rack_req['year_end'].empty?
+        @advanced_query.keyword_queries['year_end'] = rack_req['year_end']
+      end
+      @advanced_query.keyword_queries.each_pair do |field, query|
+        label = BlacklightAdvancedSearch.search_field_def_for_key(field)[:display_label]
+        content << render_constraint_element(
+            label, query,
+            :remove =>
+                catalog_index_path(remove_advanced_keyword_query(field, my_params))
+        )
+      end
+      if (@advanced_query.keyword_op == "OR" &&
+          @advanced_query.keyword_queries.length > 1)
+        content = '<span class="inclusive_or appliedFilter">' + '<span class="operator">Any of:</span>' + content + '</span>'
+      end
+
+      return content
+    end
+  end
+end
