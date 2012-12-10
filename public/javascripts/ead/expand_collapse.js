4e327087eb4f816b3b1fbf2b4cdfe418a0f61c6f
bgoodm01 <bgoodm01@UITESTSBG01.(none)>
2012-12-10 15:22:20 -0500

    Changed single quotes to double quotes to be consistent.  No change in behavior.

--- public/javascripts/ead/expand_collapse.js
+++ public/javascripts/ead/expand_collapse.js
@@ -1,17 +1,17 @@
 function toggleDisplay(imgName, firstRow, lastRow) {
   var img = $(imgName);
   var rows = $("#theTable tr");
-  var display = (img.attr('src').indexOf("button_expand.png") != -1);
+  var display = (img.attr("src").indexOf("button_expand.png") != -1);
   var imgSrc = (display ? "/assets/img/button_collapse.png" : "/assets/img/button_expand.png");
   var rowDisplay = (display ? "" : "none");
   var rowIndex;
 
-  img.attr('src', imgSrc);
+  img.attr("src", imgSrc);
 
   for (rowIndex = firstRow; rowIndex <= lastRow; rowIndex++) {
     var row = $(rows[rowIndex]);
 
-    row.css('display', rowDisplay);
+    row.css("display", rowDisplay);
   }
 }
 
@@ -26,27 +26,27 @@ function displayAll(display) {
 
   for (rowIndex = 0; rowIndex < rowCount; rowIndex++) {
     var row = $(rows[rowIndex]);
-    var className = row.attr('class');
+    var className = row.attr("class");
 
     if (className == "table_options") {
       buttonRow = row;
     } else if (className == "table_header") {
       // do nothing
     } else if (className == "folderRow") {
-      var toggler = row.find('.folderRowToggler');
+      var toggler = row.find(".folderRowToggler");
 
       if (toggler.length) {
-        toggler.attr('src', imgSrc);
+        toggler.attr("src", imgSrc);
         foldersFound = true;
       }
     } else {
-      row.css('display', rowDisplay);
+      row.css("display", rowDisplay);
     }
   }
 
   if (!foldersFound && buttonRow != null) {
     // There are no folders on this page, so hide the expand/collapse buttons.
-    buttonRow.css('display', "none");
+    buttonRow.css("display", "none");
   }
 }
 
